388302666f77a30ad7fe0908c1778e7327652088
Davin-Ricketts <davinricketts32@gmail.com>
2022-03-26 16:26:25 -0400

    Made action bar pretty

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "./index.scss";
+import AddSquare from '../../assets/add-square.svg'
+import Settings from '../../assets/settings.svg'
+import World from '../../assets/world.svg'
 function Navigation({ props }) {
   return (
+    
     <div className="Nav">
-      <a href="#home" class="active">
-        ADD
-      </a>
-      <a href="#news">MAP</a>
-      <a href="#contact">Settings</a>
+      <button className="">
+        <img src={AddSquare} alt="add"/>
+      </button>
+      <button className="map active">
+        <img src={World} alt='world' />
+      </button>
+      <button className="">
+        <img src={Settings} alt='settings' />
+      </button>
     </div>
   );
 }
