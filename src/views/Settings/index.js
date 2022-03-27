7b36a140ff76e2dee8c150870f14be4e80d0803c
esogelola <esogelola@gmail.com>
2022-03-27 11:59:39 -0400

    Added signout button

--- src/views/Settings/index.js
+++ src/views/Settings/index.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import "./index.scss";
 function Settings() {
+  const history = useHistory();
   return (
     <div className="SettingsContainer">
       <h2>Settings</h2>
@@ -13,6 +15,16 @@ function Settings() {
 
       <h2>Manage listings</h2>
       <p>No Listing Available</p>
+
+      <button
+        className="main"
+        style={{ cursor: "pointer" }}
+        onClick={() => {
+          history.push("/");
+        }}
+      >
+        Sign Out
+      </button>
     </div>
   );
 }
